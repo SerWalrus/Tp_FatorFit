fa2052b4424164ef3a58b650e8261889b2cd5f89
dsi <dsi@A0004160>
2022-01-03 14:59:15 +0100

    add user dao an co

--- src/bdd/AccessBD.java
+++ src/bdd/AccessBD.java
@@ -5,10 +5,29 @@
  */
 package bdd;
 
+import java.sql.*;
 /**
  *
  * @author dsi
  */
 public class AccessBD {
-    //au moins c'est la
+    
+    protected static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    protected static final String DB_NAME = "dev7fatfitdb";
+    protected static final String URL = "jdbc:mysql://localhost/"+DB_NAME;
+    protected static final String USER = "root";
+    protected static final String PASSWORD = "";
+    
+    static {
+        try {
+            Class.forName(DRIVER_NAME).newInstance();
+        } catch (Exception e) {
+            System.out.println("err char bd : " + e.getMessage());
+        }
+    }
+    
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    
 }
